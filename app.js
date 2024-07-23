b55d8232ee9c1700577085e62447a10670d171b2
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-07-23 15:12:07 +0500

    push

--- app.js
+++ app.js
@@ -4,51 +4,53 @@ let btn = document.getElementById("btn");
 let inputEmpty = document.getElementById("error-msg");
 let userBox = document.getElementById("show");
 
+
 btn.addEventListener("click" , (e) =>{
  e.preventDefault();
  let name = userName.value;
  let gender = userGender.value;
- let rollNum = Math.floor(Math.random() * 1000000);
-
- if (userName === '' || userGender === '') {
-  errorMsg.textContent == 'Please fill in all fields';
-  return;
+ let rollNumber = Math.floor(Math.random() * 1000000);
+ if (name == '' || gender == '') {
+  inputEmpty.textContent = 'Please fill in all fields';
  }
+ else{
+    userBox.innerHTML += `
+    <li>
+    <h1>Name</h1>
+    <p>${name}</p>
+    <h1>Gender</h1>
+    <p>${gender}</p>
+    <h1>Roll No</h1>
+    <p>${rollNumber}</p>
+    <button class="edit">Edit</button>
+    <button class="delete">Delete</button>
+    </li>
+    ` ;
+ }
+ let del_btn = document.querySelectorAll(".delete");
+ del_btn.forEach((btn) => {
+   btn.addEventListener("click", () => {
+     btn.parentNode.remove();
+   });
+ });
+ let edit_btn = document.querySelectorAll(".edit");
+ edit_btn.forEach((btn) => {
+   btn.addEventListener("click", () => {
+       let cname= prompt("name")
+       let cgender = prompt("gender")
+       btn.parentNode.children[1].innerHTML = cname
+       btn.parentNode.children[5].innerHTML= cgender
+   });
+ });
 });
-// btn.addEventListener("click" , () => {
+
      
 
-//       userBox.innerHTML += `
-//       <li>
-//       <h1>Name</h1>
-//       <p>${name}</p>
-//       <h1>Gender</h1>
-//       <p>${gender}</p>
-//       <h1>Roll No</h1>
-//       <p>${rollNum}</p>
-//       <button class="edit">Edit</button>
-//       <button class="delete">Delete</button>
-//       </li>
-//       ` ;
-//       let del_btn = document.querySelectorAll(".delete");
-//       del_btn.forEach((btn) => {
-//         btn.addEventListener("click", () => {
-//           btn.parentNode.remove();
-//         });
-//       });
+    
+  
 
             
-//   let edit_btn = document.querySelectorAll(".edit");
-//   edit_btn.forEach((btn) => {
-//     btn.addEventListener("click", () => {
-//         let cname= prompt("name")
-//         let cemail = prompt("email")
-//         let cgender = prompt("gender")
-//         btn.parentNode.children[1].innerHTML = cname
-//         // btn.parentNode.children[3].innerHTML= cemail
-//         btn.parentNode.children[5].innerHTML= cgender
-//     });
-//   });
+
        
 // });
 
